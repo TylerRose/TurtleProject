272b2ced214e9fc33c7ddd4ec883a0fdc62b53d7
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2021-12-27 00:09:32 -0800

    Update update.lua

--- update.lua
+++ update.lua
@@ -15,7 +15,7 @@ shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject
 
 --Programs from github
 print("Getting programs...")
-shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/programs/dig16x16x-6.lua", "/disk/programs/dig16x16x-6.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/programs/dig16x16x-6.lua", loc.."programs/dig16x16x-6.lua")
 
 
 --Root dir from github
