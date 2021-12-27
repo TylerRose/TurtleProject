8cb0172f0f4879c0aee47a3c7026eb8c1d78c382
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2021-12-26 22:31:59 -0800

    Auto Update

---
+++ Update.lua
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/move.lua /disk/modules/move.lua
+wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/dump.lua /disk/modules/dump.lua
+wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/echo.lua /disk/modules/echo.lua
+wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/include.lua /disk/include.lua
