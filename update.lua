7735a51f93a4bb35b9a673b19d4d71dada5a9686
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2021-12-26 23:35:50 -0800

    Update update.lua

--- update.lua
+++ update.lua
@@ -2,15 +2,18 @@
 shell.execute("rm","/disk/*")
 
 --Modules from github
+print("Getting modules...")
 shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/move.lua", "/disk/modules/move.lua")
 shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/dump.lua", "/disk/modules/dump.lua")
 shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/echo.lua", "/disk/modules/echo.lua")
 
 --Programs from github
+print("Getting programs...")
 shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/programs/dig16x16x-6.lua", "/disk/programs/dig16x16x-6.lua")
 
 
 --Root dir from github
+print("Getting root files...")
 shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/updater.lua", "/disk/updater.lua")
 shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/.settings.lua", "/disk/.settings.lua")
 shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/include.lua", "/disk/include.lua")
