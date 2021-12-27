219b986bdcb24e68c261c15578418eefa4ca0197
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2021-12-26 23:18:51 -0800

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -2,10 +2,12 @@ fs.delete("/.settings")
 fs.delete("/programs")
 fs.delete("/modules")
 fs.delete("/include.lua")
+fs.delete("/updater.lua")
 fs.copy("disk/.settings", ".settings")
 fs.copy("disk/programs/", "programs")
 fs.copy("disk/modules/", "modules")
 fs.copy("disk/include.lua", "include.lua")
+fs.copy("disk/updater.lua", "updater.lua")
 
 if not fs.exists("startup.lua") then
     fs.copy("disk/turtleStartup.lua", "startup.lua")
