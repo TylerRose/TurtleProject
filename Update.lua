030cb158cd46ebb1d39a9d927bd9f56b0fd4ea2d
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2021-12-26 22:49:02 -0800

    Auto Update

--- Update.lua
+++ Update.lua
@@ -1,9 +1,9 @@
 --Modules from github
-wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/move.lua /disk/modules/move.lua
-wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/dump.lua /disk/modules/dump.lua
-wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/echo.lua /disk/modules/echo.lua
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/move.lua", "/disk/modules/move.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/dump.lua", "/disk/modules/dump.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/echo.lua", "/disk/modules/echo.lua")
 
 --Programs from github
 
 --Root dir from github
-wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/include.lua /disk/include.lua
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/include.lua", "/disk/include.lua")
