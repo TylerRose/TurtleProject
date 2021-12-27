eb389e2c8285daab9d8da57b1ac8a469db7c80db
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2021-12-26 22:37:48 -0800

    Update Update.lua

--- Update.lua
+++ Update.lua
@@ -1,4 +1,9 @@
-wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/move.lua /disk/modules/move.lua
-wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/dump.lua /disk/modules/dump.lua
-wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/echo.lua /disk/modules/echo.lua
+--Modules from github
+wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/move.lua /disk/modules/move.lua
+wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/dump.lua /disk/modules/dump.lua
+wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/echo.lua /disk/modules/echo.lua
+
+--Programs from github
+
+--Root dir from github
 wget https://raw.githubusercontent.com/TylerRose/TurtleProject/main/include.lua /disk/include.lua
