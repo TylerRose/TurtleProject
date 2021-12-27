08a13d61f784fd91f7e8ec4e186a3a7d23669968
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2021-12-26 23:54:01 -0800

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,11 +1,18 @@
+--Get location to update to
+loc = "/"
+if fs.exists("/disk/") then
+	loc = "/disk/"
+end
+
 --Cleanup
-shell.execute("rm","/disk/*")
+shell.execute("rm","loc.."*")
+shell.execute("rm","loc.."*")
 
 --Modules from github
 print("Getting modules...")
-shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/move.lua", "/disk/modules/move.lua")
-shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/dump.lua", "/disk/modules/dump.lua")
-shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/echo.lua", "/disk/modules/echo.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/move.lua", loc.."modules/move.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/dump.lua", loc.."modules/dump.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/modules/echo.lua", loc.."modules/echo.lua")
 
 --Programs from github
 print("Getting programs...")
@@ -14,9 +21,9 @@ shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject
 
 --Root dir from github
 print("Getting root files...")
-shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/updater.lua", "/disk/updater.lua")
-shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/.settings", "/disk/.settings")
-shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/include.lua", "/disk/include.lua")
-shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/startup.lua", "/disk/startup.lua")
-shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/turtleStartup.lua", "/disk/turtleStartup.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/updater.lua", loc.."updater.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/.settings", loc..".settings")
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/include.lua", loc.."include.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/startup.lua", loc.."startup.lua")
+shell.execute("wget", "https://raw.githubusercontent.com/TylerRose/TurtleProject/main/turtleStartup.lua", loc.."turtleStartup.lua")
 
