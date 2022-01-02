a29e4b135bd1156d0f6f79062d462a0b4656ed8f
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2022-01-01 17:19:51 -0800

    Update dump.lua

--- modules/dump.lua
+++ modules/dump.lua
@@ -1,10 +1,21 @@
-function slots(a, b)
+function slotsFront(a, b)
     for i = a, b do
         turtle.select(i)
         turtle.drop()
     end
 end
 
-function current()
+function slotsDown(a, b)
+    for i = a, b do
+        turtle.select(i)
+        turtle.dropDown()
+    end
+end
+
+function currentFront()
     turlte.drop()
+end
+
+function currentDown()
+    turlte.dropDown()
 end
