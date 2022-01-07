da3f8ae25d9bd8a6d6b6dbb9823bb05f9d2d550e
Tyler Rose <47163327+TylerRose@users.noreply.github.com>
2022-01-06 16:06:46 -0800

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -3,13 +3,18 @@ fs.delete("/programs")
 fs.delete("/modules")
 fs.delete("/include.lua")
 fs.delete("/updater.lua")
-fs.copy("disk/.settings", ".settings")
-fs.copy("disk/programs/", "programs")
-fs.copy("disk/modules/", "modules")
-fs.copy("disk/include.lua", "include.lua")
-fs.copy("disk/updater.lua", "updater.lua")
+if fs.exists("/disk/") then
+    fs.copy("disk/.settings", ".settings")
+    fs.copy("disk/programs/", "programs")
+    fs.copy("disk/modules/", "modules")
+    fs.copy("disk/include.lua", "include.lua")
+    fs.copy("disk/updater.lua", "updater.lua")
+end
 
 if not fs.exists("startup.lua") then
     fs.copy("disk/turtleStartup.lua", "startup.lua")
 end
-shell.execute("startup.lua")
+
+if fs.exists("/disk/startup.lua") then
+    shell.execute("startup.lua")
+end
